462e91333935aa6610c36d59b6f829b8a23a73e8
harperkdavis <davish@catlin.edu>
2021-07-02 22:09:10 -0700

    Delete all previous netcode

--- src/main/BordersiteServer.java
+++ src/main/BordersiteServer.java
@@ -1,24 +1,15 @@
 package main;
 
-import engine.math.ByteUtil;
-import game.Player;
-import net.Server;
+import org.json.simple.JSONObject;
 
 import java.net.InetAddress;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class BordersiteServer implements Runnable {
 
-    private Server socketServer;
     private Thread server;
-
-    public ConcurrentMap<InetAddress, Player> players = new ConcurrentHashMap<>();
-
-    private int nextPID = 0;
 
     public static void main(String[] args) {
         new BordersiteServer().start();
@@ -26,27 +17,14 @@ public class BordersiteServer implements Runnable {
 
     public void start() {
         System.out.println("[INFO] Starting server...");
-        socketServer = new Server(this);
-        socketServer.start();
+
         server = new Thread(this, "server");
         server.start();
+
     }
 
-    public void init() {
-        System.out.println("[INFO] Server started successfully!");
-    }
-
+    @Override
     public void run() {
-        init();
-        while (true) {
-            socketServer.sendPlayerData();
-        }
+
     }
-
-    public int getNextPID() {
-        nextPID++;
-        return nextPID;
-    }
-
-
 }
